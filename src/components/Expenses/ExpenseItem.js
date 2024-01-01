ae2d5c6a9ce6d744127d2cbad15e428b4f22cdfc
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-01-01 20:17:36 +0530

    file structure

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseItem(props) {
   return (
