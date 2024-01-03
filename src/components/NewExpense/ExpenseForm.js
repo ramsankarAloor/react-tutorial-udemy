23ce80786334536e19be716870317178b1265dbf
ramsankarAloor <ramsankar.aloor@gmail.com>
2024-01-03 08:47:05 +0530

    2 way binding, without this the value in the input boxes wont be cleared after we click submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -28,21 +28,28 @@ function ExpenseForm() {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
     <form className="new-expense__controls" onSubmit={submitHandler}>
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" onChange={titleChangeHandler} />
+        <input type="text" value={enterdTitle} onChange={titleChangeHandler} />
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" onChange={amountChangeHandler} />
+        <input
+          type="number"
+          value={enteredAmount}
+          onChange={amountChangeHandler}
+        />
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" onChange={dateChangeHandler} />
+        <input type="date" value={enteredDate} onChange={dateChangeHandler} />
       </div>
       <div className="new-expense__actions">
         <button type="submit">Add Expense</button>
